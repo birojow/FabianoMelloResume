6ad0984c79e9c60b63b0d9d0aa33f10375447102
Fabiano Mello <f.augmello@hotmail.com>
2025-08-10 15:34:23 -0300

    add language selection screen

--- app/src/main/java/app/birojow/fabianomelloresume/MainActivity.kt
+++ app/src/main/java/app/birojow/fabianomelloresume/MainActivity.kt
@@ -1,16 +1,12 @@
 package app.birojow.fabianomelloresume
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.Surface
+import app.birojow.fabianomelloresume.ui.onboarding.LanguageSelectionScreen
 import app.birojow.fabianomelloresume.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    LanguageSelectionScreen {
+                        Toast.makeText(
+                            this,
+                            it.name,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/app/birojow/fabianomelloresume/ui/onboarding/LanguageSelectionScreen.kt
+++ app/src/main/java/app/birojow/fabianomelloresume/ui/onboarding/LanguageSelectionScreen.kt
@@ -1,16 +1,115 @@
 package app.birojow.fabianomelloresume.ui.onboarding
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import app.birojow.fabianomelloresume.R
+import app.birojow.fabianomelloresume.domain.AppLanguage
+import app.birojow.fabianomelloresume.ui.theme.AppTheme
 
 @Composable
-internal fun LanguageSelectionScreen(modifier: Modifier = Modifier) {
+internal fun LanguageSelectionScreen(
+    modifier: Modifier = Modifier,
+    onLanguageSelection: (AppLanguage) -> Unit
+) {
+    val spacingBetweenEmojiAndName = 16.dp
+    val textStyle = MaterialTheme.typography.displaySmall
 
+    Column(
+        modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(spacingBetweenEmojiAndName),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable(
+                    role = Role.Button,
+                    onClick = { onLanguageSelection(AppLanguage.EN) }
+                )
+        ) {
+            Text(
+                text = stringResource(R.string.great_britain_flag_emoji),
+                style = textStyle
+            )
+
+            Text(
+                text = stringResource(R.string.english_language),
+                style = textStyle
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(spacingBetweenEmojiAndName),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable(
+                    role = Role.Button,
+                    onClick = { onLanguageSelection(AppLanguage.PT) }
+                )
+        ) {
+            Text(
+                text = stringResource(R.string.portugal_flag_emoji),
+                style = textStyle
+            )
+
+            Text(
+                text = stringResource(R.string.portuguese_language),
+                style = textStyle
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(spacingBetweenEmojiAndName),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable(
+                    role = Role.Button,
+                    onClick = { onLanguageSelection(AppLanguage.ES) }
+                )
+        ) {
+            Text(
+                text = stringResource(R.string.spain_flag_emoji),
+                style = textStyle
+            )
+
+            Text(
+                text = stringResource(R.string.espanol_language),
+                style = textStyle
+            )
+        }
+    }
 }
 
 @Preview
 @Composable
-private fun LanguageSelectionScreenPreview() {
-    LanguageSelectionScreen()
+private fun LanguageSelectionScreenLightPreview() {
+    AppTheme(darkTheme = false) {
+        Surface {
+            LanguageSelectionScreen() {}
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun LanguageSelectionScreenDarkPreview() {
+    AppTheme(darkTheme = true) {
+        Surface {
+            LanguageSelectionScreen() {}
+        }
+    }
 }
