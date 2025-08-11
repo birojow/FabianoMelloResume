47e49aeecc99ebca471b5527a6ef8050e1463b8d
Fabiano Mello <f.augmello@hotmail.com>
2025-08-11 15:38:02 -0300

    refactor language selection screen

--- app/src/main/java/app/birojow/fabianomelloresume/ui/onboarding/LanguageSelectionScreen.kt
+++ app/src/main/java/app/birojow/fabianomelloresume/ui/onboarding/LanguageSelectionScreen.kt
@@ -5,17 +5,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import app.birojow.fabianomelloresume.R
 import app.birojow.fabianomelloresume.domain.AppLanguage
 import app.birojow.fabianomelloresume.ui.theme.AppTheme
 
@@ -24,73 +26,63 @@ fun LanguageSelectionScreen(
     modifier: Modifier = Modifier,
     onLanguageSelection: (AppLanguage) -> Unit
 ) {
-    val spacingBetweenEmojiAndName = 16.dp
-    val textStyle = MaterialTheme.typography.displaySmall
-
     Column(
         modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(spacingBetweenEmojiAndName),
-            verticalAlignment = Alignment.CenterVertically,
+        FlagAndLanguage(
+            language = AppLanguage.EN,
+            onLanguageSelection = onLanguageSelection,
+        )
+
+        FlagAndLanguage(
+            language = AppLanguage.PT,
+            onLanguageSelection = onLanguageSelection,
+        )
+
+        FlagAndLanguage(
+            language = AppLanguage.ES,
+            onLanguageSelection = onLanguageSelection,
+        )
+    }
+}
+
+@Composable
+private fun FlagAndLanguage(
+    language: AppLanguage,
+    onLanguageSelection: (AppLanguage) -> Unit
+) {
+    val spacingBetweenEmojiAndName = 16.dp
+    val flagAndTextPadding = 24.dp
+    val interactionShape = RoundedCornerShape(50.dp)
+    val textStyle = MaterialTheme.typography.displaySmall
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(spacingBetweenEmojiAndName),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clip(interactionShape)
+            .clickable(
+                role = Role.Button,
+                onClick = { onLanguageSelection(language) }
+            )
+    ) {
+        Text(
+            text = stringResource(language.flagEmojiId),
+            style = textStyle,
             modifier = Modifier
-                .clickable(
-                    role = Role.Button,
-                    onClick = { onLanguageSelection(AppLanguage.EN) }
-                )
-        ) {
-            Text(
-                text = stringResource(R.string.great_britain_flag_emoji),
-                style = textStyle
-            )
+                .padding(vertical = flagAndTextPadding)
+                .padding(start = flagAndTextPadding)
+        )
 
-            Text(
-                text = stringResource(R.string.english_language),
-                style = textStyle
-            )
-        }
-
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(spacingBetweenEmojiAndName),
-            verticalAlignment = Alignment.CenterVertically,
+        Text(
+            text = stringResource(language.languageNameId),
+            style = textStyle,
             modifier = Modifier
-                .clickable(
-                    role = Role.Button,
-                    onClick = { onLanguageSelection(AppLanguage.PT) }
-                )
-        ) {
-            Text(
-                text = stringResource(R.string.portugal_flag_emoji),
-                style = textStyle
-            )
-
-            Text(
-                text = stringResource(R.string.portuguese_language),
-                style = textStyle
-            )
-        }
-
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(spacingBetweenEmojiAndName),
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .clickable(
-                    role = Role.Button,
-                    onClick = { onLanguageSelection(AppLanguage.ES) }
-                )
-        ) {
-            Text(
-                text = stringResource(R.string.spain_flag_emoji),
-                style = textStyle
-            )
-
-            Text(
-                text = stringResource(R.string.espanol_language),
-                style = textStyle
-            )
-        }
+                .padding(vertical = flagAndTextPadding)
+                .padding(end = flagAndTextPadding)
+        )
     }
 }
 
@@ -99,7 +91,7 @@ fun LanguageSelectionScreen(
 private fun LanguageSelectionScreenLightPreview() {
     AppTheme(darkTheme = false) {
         Surface {
-            LanguageSelectionScreen() {}
+            LanguageSelectionScreen {}
         }
     }
 }
@@ -109,7 +101,7 @@ private fun LanguageSelectionScreenLightPreview() {
 private fun LanguageSelectionScreenDarkPreview() {
     AppTheme(darkTheme = true) {
         Surface {
-            LanguageSelectionScreen() {}
+            LanguageSelectionScreen {}
         }
     }
 }
